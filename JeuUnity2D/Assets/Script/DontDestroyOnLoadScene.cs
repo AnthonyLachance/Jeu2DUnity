c7f6806f0d7c9fc0ab66c4554b74080589e91a4b
AnthonyLachance <101238048+AnthonyLachance@users.noreply.github.com>
2022-03-11 12:33:59 -0500

    Plus de niveau !
    Ajout d'une porte et de la fonctionnalité de changer d'une scene a une autre, ce qui permet de créer plusieurs niveau !

---
+++ JeuUnity2D/Assets/Script/DontDestroyOnLoadScene.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DontDestroyOnLoadScene : MonoBehaviour
+{
+
+    public GameObject[] objects;
+
+
+    void Awake()
+    {
+        foreach(var element in objects)
+        {
+            DontDestroyOnLoad(element);
+        }
+
+    }
+ 
+}
