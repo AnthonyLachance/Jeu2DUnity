1de336c6ea0f267a7c4c1841e47d35939c006342
AnthonyLachance <101238048+AnthonyLachance@users.noreply.github.com>
2022-03-10 19:37:37 -0500

    Ajout de la fonction piece
    J'ai ajouter une préfab de coins, qui permet davoir une piece dans le niveau. Et aussi une l'ajout d'un inventaire qui compte mes pieces.

---
+++ JeuUnity2D/Assets/Script/Inventory.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public int coinsCount;
+    public Text coinsCountText;
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        // Code d'erreur si jamais ya un bug et qu'il y a 2 inventaire
+        if(instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de Inventory dans la scène");
+        }
+
+        instance = this;
+    }
+
+    public void AddCoins(int count)
+    {
+        coinsCount += count;
+        coinsCountText.text = coinsCount.ToString();
+    }
+}
