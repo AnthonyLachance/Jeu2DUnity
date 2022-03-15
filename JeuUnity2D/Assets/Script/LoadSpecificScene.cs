0c576299764ce34303656dc801552530fae55d54
AnthonyLachance <101238048+AnthonyLachance@users.noreply.github.com>
2022-03-15 15:33:00 -0400

    Fonctionnalité : Coffre
    Ajout d'une fonctionnalité ou le joueur peut ouvrir un coffre et avoir des pieces.
    Et ajout de devoir appuyé sur E pour changer de niveau.

--- JeuUnity2D/Assets/Script/LoadSpecificScene.cs
+++ JeuUnity2D/Assets/Script/LoadSpecificScene.cs
@@ -1,31 +1,53 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadSpecificScene : MonoBehaviour
 {
+    private bool isInRange;
     public string sceneName;
     public Animator fadeSystem;
+    public Text interactUI;
 
     private void Awake()
     {
         fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+        interactUI = GameObject.FindGameObjectWithTag("InteractUI").GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        if (isInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(loadNextScene());
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            isInRange = true;
+            interactUI.enabled = true;
+        }
+    }
 
-            StartCoroutine(loadNextScene());
-            
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+            interactUI.enabled = false;
         }
     }
 
     public IEnumerator loadNextScene()
     {
-        fadeSystem.SetTrigger("FadeIn");
+        
+        fadeSystem.SetTrigger("FadeIn");     
         yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(sceneName);
+        
     }
 }
