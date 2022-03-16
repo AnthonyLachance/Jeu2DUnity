c827e0abe9c5b33aee7be8ce54a56f1ede934286
AnthonyLachance <101238048+AnthonyLachance@users.noreply.github.com>
2022-03-16 18:09:20 -0400

    Ajout complet du systeme de choix de niveau et de sauvegarde des données des niveaux

--- Assets/Script/LoadAndSaveData.cs
+++ Assets/Script/LoadAndSaveData.cs
@@ -20,15 +20,22 @@ public class LoadAndSaveData : MonoBehaviour
         Inventory.instance.coinsCount = PlayerPrefs.GetInt("coinsCount", 0);
         Inventory.instance.UpdateTextUI();
 
-       //int currentHealth = PlayerPrefs.GetInt("playerHealth", PlayerHealth.instance.maxHealth);
-       //PlayerHealth.instance.currentHealth = currentHealth;
-       //PlayerHealth.instance.healthBar.SetHealth(currentHealth);
+        //Code pour sauvgarder la vie du joueur entre les niveau
+        //int currentHealth = PlayerPrefs.GetInt("playerHealth", PlayerHealth.instance.maxHealth);
+        //PlayerHealth.instance.currentHealth = currentHealth;
+        //PlayerHealth.instance.healthBar.SetHealth(currentHealth);
     }
 
     public void SaveData()
     {
         PlayerPrefs.SetInt("coinsCount", Inventory.instance.coinsCount);
-       // PlayerPrefs.SetInt("playerHealth", PlayerHealth.instance.currentHealth);
+
+        if(CurrentSceneManager.instance.levelToUnlock > PlayerPrefs.GetInt("levelReached", 1))
+        {
+            PlayerPrefs.SetInt("levelReached", CurrentSceneManager.instance.levelToUnlock);
+        }
+        //Code pour sauvgarder la vie du joueur entre les niveau
+        // PlayerPrefs.SetInt("playerHealth", PlayerHealth.instance.currentHealth);
 
     }
    
