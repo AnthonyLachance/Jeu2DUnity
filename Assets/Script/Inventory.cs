1ae7a6995b428a719618e3116e127db7f49802e0
AnthonyLachance <101238048+AnthonyLachance@users.noreply.github.com>
2022-03-17 17:08:36 -0400

    Systeme d'inventaire completé
    Version presque final du system d'inventaire et de consumable

--- Assets/Script/Inventory.cs
+++ Assets/Script/Inventory.cs
@@ -10,8 +10,10 @@ public class Inventory : MonoBehaviour
 
     public List<Item> content = new List<Item>();
     public int contentCurrentIndex = 0;
-
+    public Image itemImageUI;
+    public Text itemNameUI;
     public static Inventory instance;
+    public Sprite emptyItemImage;
 
     private void Awake()
     {
@@ -24,30 +26,70 @@ public class Inventory : MonoBehaviour
         instance = this;
     }
 
+    private void Start()
+    {
+        UpdateInventoryUI();
+    }
+
     public void GetNextItem()
     {
-        contentCurrentIndex++;
+        if (content.Count == 0)
+        {
+            return;
+        }
+
+        contentCurrentIndex++; 
         if (contentCurrentIndex > content.Count - 1)
         {
             contentCurrentIndex = 0;
         }
+        UpdateInventoryUI();
     }
 
     public void GetPreviousItem()
     {
+        if (content.Count == 0)
+        {
+            return;
+        }
+
         contentCurrentIndex--;
-        if (contentCurrentIndex < content.Count - 1)
+        if (contentCurrentIndex < 0)
         {
             contentCurrentIndex = content.Count -1;
         }
+        UpdateInventoryUI();
     }
+
+
+    public void UpdateInventoryUI()
+    {
+        if (content.Count > 0)
+        {
+            itemNameUI.text = content[contentCurrentIndex].name;
+            itemImageUI.sprite = content[contentCurrentIndex].image;
+        }
+        else
+        {
+            itemNameUI.text = "";
+            itemImageUI.sprite = emptyItemImage;
+        }
+    }
+
 
     public void ConsumeItem()
     {
+        if(content.Count == 0)
+        {
+            return;
+        }
+
         Item currentItem = content[0];
         PlayerHealth.instance.HealPlayer(currentItem.hpGiven);
         PlayerMovement.instance.moveSpeed += currentItem.speedGiven;
         content.Remove(currentItem);
+        GetNextItem();
+        UpdateInventoryUI();
     }
 
     public void AddCoins(int count)
@@ -64,6 +106,8 @@ public class Inventory : MonoBehaviour
 
     public void UpdateTextUI()
     {
+
+
         coinsCountText.text = coinsCount.ToString();
     }
 }
